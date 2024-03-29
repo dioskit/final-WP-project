5fdcab71d6e868b48731792f2e4892548da8ae8c
dioskit <154680296+dioskit@users.noreply.github.com>
2024-03-30 00:30:21 +0530

    dark theme sourcce is added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("code running! :)");
+const toggleButton = document.getElementById('toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
+});
+
+// Check the local storage for user preference
+const theme = localStorage.getItem('theme');
+if (theme === 'dark') {
+  body.classList.add('dark-mode');
+}
